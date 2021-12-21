bc297f6eb813977e5489d173a47d1f671692a656
shubham <devshubhamyadav@gmail.com>
2021-12-21 10:17:59 -0500

    Add project files.

---
+++ AppRunner/Form1.Designer.vb
@@ -0,0 +1,194 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.txtAppName = New System.Windows.Forms.TextBox()
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        Me.btnCloseApp = New System.Windows.Forms.Button()
+        Me.lstApp = New System.Windows.Forms.ListBox()
+        Me.btnClear = New System.Windows.Forms.Button()
+        Me.btnAppRun = New System.Windows.Forms.Button()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.GroupBox2 = New System.Windows.Forms.GroupBox()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.ListBox1 = New System.Windows.Forms.ListBox()
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.GroupBox1.SuspendLayout()
+        Me.GroupBox2.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'txtAppName
+        '
+        Me.txtAppName.Location = New System.Drawing.Point(161, 42)
+        Me.txtAppName.Margin = New System.Windows.Forms.Padding(4, 4, 4, 4)
+        Me.txtAppName.Name = "txtAppName"
+        Me.txtAppName.Size = New System.Drawing.Size(240, 22)
+        Me.txtAppName.TabIndex = 0
+        '
+        'GroupBox1
+        '
+        Me.GroupBox1.BackColor = System.Drawing.SystemColors.ControlDark
+        Me.GroupBox1.Controls.Add(Me.btnCloseApp)
+        Me.GroupBox1.Controls.Add(Me.lstApp)
+        Me.GroupBox1.Controls.Add(Me.btnClear)
+        Me.GroupBox1.Controls.Add(Me.btnAppRun)
+        Me.GroupBox1.Controls.Add(Me.Label1)
+        Me.GroupBox1.Controls.Add(Me.txtAppName)
+        Me.GroupBox1.Location = New System.Drawing.Point(16, 15)
+        Me.GroupBox1.Margin = New System.Windows.Forms.Padding(4, 4, 4, 4)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Padding = New System.Windows.Forms.Padding(4, 4, 4, 4)
+        Me.GroupBox1.Size = New System.Drawing.Size(468, 540)
+        Me.GroupBox1.TabIndex = 1
+        Me.GroupBox1.TabStop = False
+        Me.GroupBox1.Text = "Application Runner"
+        '
+        'btnCloseApp
+        '
+        Me.btnCloseApp.Location = New System.Drawing.Point(161, 480)
+        Me.btnCloseApp.Margin = New System.Windows.Forms.Padding(4, 4, 4, 4)
+        Me.btnCloseApp.Name = "btnCloseApp"
+        Me.btnCloseApp.Size = New System.Drawing.Size(100, 28)
+        Me.btnCloseApp.TabIndex = 5
+        Me.btnCloseApp.Text = "Close App"
+        Me.btnCloseApp.UseVisualStyleBackColor = True
+        '
+        'lstApp
+        '
+        Me.lstApp.FormattingEnabled = True
+        Me.lstApp.ItemHeight = 16
+        Me.lstApp.Location = New System.Drawing.Point(161, 150)
+        Me.lstApp.Margin = New System.Windows.Forms.Padding(4, 4, 4, 4)
+        Me.lstApp.Name = "lstApp"
+        Me.lstApp.Size = New System.Drawing.Size(240, 308)
+        Me.lstApp.TabIndex = 4
+        '
+        'btnClear
+        '
+        Me.btnClear.Location = New System.Drawing.Point(303, 92)
+        Me.btnClear.Margin = New System.Windows.Forms.Padding(4, 4, 4, 4)
+        Me.btnClear.Name = "btnClear"
+        Me.btnClear.Size = New System.Drawing.Size(100, 28)
+        Me.btnClear.TabIndex = 3
+        Me.btnClear.Text = "Clear"
+        Me.btnClear.UseVisualStyleBackColor = True
+        '
+        'btnAppRun
+        '
+        Me.btnAppRun.Location = New System.Drawing.Point(161, 92)
+        Me.btnAppRun.Margin = New System.Windows.Forms.Padding(4, 4, 4, 4)
+        Me.btnAppRun.Name = "btnAppRun"
+        Me.btnAppRun.Size = New System.Drawing.Size(100, 28)
+        Me.btnAppRun.TabIndex = 2
+        Me.btnAppRun.Text = "Run App"
+        Me.btnAppRun.UseVisualStyleBackColor = True
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(25, 46)
+        Me.Label1.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(112, 17)
+        Me.Label1.TabIndex = 1
+        Me.Label1.Text = "Enter App Name"
+        '
+        'GroupBox2
+        '
+        Me.GroupBox2.BackColor = System.Drawing.SystemColors.ControlDark
+        Me.GroupBox2.Controls.Add(Me.Button1)
+        Me.GroupBox2.Controls.Add(Me.ListBox1)
+        Me.GroupBox2.Controls.Add(Me.Button3)
+        Me.GroupBox2.Location = New System.Drawing.Point(600, 15)
+        Me.GroupBox2.Margin = New System.Windows.Forms.Padding(4, 4, 4, 4)
+        Me.GroupBox2.Name = "GroupBox2"
+        Me.GroupBox2.Padding = New System.Windows.Forms.Padding(4, 4, 4, 4)
+        Me.GroupBox2.Size = New System.Drawing.Size(359, 540)
+        Me.GroupBox2.TabIndex = 2
+        Me.GroupBox2.TabStop = False
+        Me.GroupBox2.Text = "System Processes"
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(180, 353)
+        Me.Button1.Margin = New System.Windows.Forms.Padding(4, 4, 4, 4)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(100, 28)
+        Me.Button1.TabIndex = 5
+        Me.Button1.Text = "Close App"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'ListBox1
+        '
+        Me.ListBox1.FormattingEnabled = True
+        Me.ListBox1.ItemHeight = 16
+        Me.ListBox1.Location = New System.Drawing.Point(39, 23)
+        Me.ListBox1.Margin = New System.Windows.Forms.Padding(4, 4, 4, 4)
+        Me.ListBox1.Name = "ListBox1"
+        Me.ListBox1.Size = New System.Drawing.Size(240, 308)
+        Me.ListBox1.TabIndex = 4
+        '
+        'Button3
+        '
+        Me.Button3.Location = New System.Drawing.Point(39, 353)
+        Me.Button3.Margin = New System.Windows.Forms.Padding(4, 4, 4, 4)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(133, 28)
+        Me.Button3.TabIndex = 2
+        Me.Button3.Text = "List Processes"
+        Me.Button3.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(979, 588)
+        Me.Controls.Add(Me.GroupBox2)
+        Me.Controls.Add(Me.GroupBox1)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.Margin = New System.Windows.Forms.Padding(4, 4, 4, 4)
+        Me.MaximizeBox = False
+        Me.Name = "Form1"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Application Runner"
+        Me.GroupBox1.ResumeLayout(False)
+        Me.GroupBox1.PerformLayout()
+        Me.GroupBox2.ResumeLayout(False)
+        Me.ResumeLayout(False)
+
+    End Sub
+    Friend WithEvents txtAppName As System.Windows.Forms.TextBox
+    Friend WithEvents GroupBox1 As System.Windows.Forms.GroupBox
+    Friend WithEvents btnAppRun As System.Windows.Forms.Button
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents btnClear As System.Windows.Forms.Button
+    Friend WithEvents btnCloseApp As System.Windows.Forms.Button
+    Friend WithEvents lstApp As System.Windows.Forms.ListBox
+    Friend WithEvents GroupBox2 As System.Windows.Forms.GroupBox
+    Friend WithEvents Button1 As System.Windows.Forms.Button
+    Friend WithEvents ListBox1 As System.Windows.Forms.ListBox
+    Friend WithEvents Button3 As System.Windows.Forms.Button
+
+End Class
---
+++ AppRunner/Form1.vb
@@ -0,0 +1,46 @@
+﻿Public Class Form1
+
+    Private Sub btnAppRun_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnAppRun.Click
+        If txtAppName.Text = "" Then
+            MsgBox("Please Enter any App Name First.", vbOKCancel + MsgBoxStyle.Exclamation, "VVIT")
+            txtAppName.BackColor = Color.LightPink
+            txtAppName.Focus()
+            Exit Sub
+        End If
+        Dim pid = Shell(txtAppName.Text, AppWinStyle.NormalFocus)
+        lstApp.Items.Add(pid)
+        'MsgBox(pid)
+
+    End Sub
+
+    Private Sub txtAppName_KeyPress(ByVal sender As Object, ByVal e As System.Windows.Forms.KeyPressEventArgs) Handles txtAppName.KeyPress
+        txtAppName.BackColor = Color.White
+    End Sub
+
+    
+    Private Sub btnClear_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnClear.Click
+        txtAppName.Text = ""
+        txtAppName.Focus()
+    End Sub
+
+    Private Sub btnCloseApp_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnCloseApp.Click
+        If lstApp.Items.Count = 0 Then
+            MsgBox("There is no one App in App List.")
+            Exit Sub
+        End If
+        If lstApp.SelectedIndex < 0 Then
+            MsgBox("Please select any App in App List.")
+            Exit Sub
+        End If
+        If MsgBox("Are you sure to close the app?", vbYesNo + vbCritical, "VVIT") = MsgBoxResult.Yes Then
+            Process.GetProcessById(lstApp.SelectedItem).Kill()
+            lstApp.Items.Remove(lstApp.SelectedItem)
+        End If
+
+
+    End Sub
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+
+    End Sub
+End Class
